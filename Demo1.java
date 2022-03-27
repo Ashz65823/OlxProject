b27bc22876c93b1c0a4407b4aaff2be7b93be8ac
Ashz65823 <ashwinipapu04@gmail.com>
2022-03-28 01:22:57 +0530

    OLX Security Project

---
+++ Demo1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Demo1
+{
+public static void main(String[] args )
+{
+System.out.println("Welcome to java");
+}
+}
+
+class Demo2
+{
+public static void main(String[] args )
+{
+System.out.println("We are starting to learn java");
+}
+}
+
+class Demo3
+{
+public static void main(String[] args )
+{
+System.out.println("Hope everything works fine");
+}
+}
+class Demo4
+{
+void go()
+{
+System.out.println("Hello");
+//will not execute because main is not found.. this is not application class.. 
+}
+}
